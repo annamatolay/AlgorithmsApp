d4bc64e746265af15454ba99764ae45de701f39c
matolaypal <matolaypal@gmail.com>
2017-04-26 00:18:36 +0200

    ui ok, run bubble

---
+++ AlgorithmsApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using AlgorithmsApp.sorting;
+
+namespace AlgorithmsApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! {0} BuubleSort - buble {0} Exit is q", Environment.NewLine);
+//            while(true)
+            for(;;)
+            {
+                Console.WriteLine("Give a correct operation name, please!");
+                var op = Console.ReadLine();
+                switch (op)
+                {
+                    case "q":
+                        Environment.Exit(0);
+                        break;
+                    case "clear":
+                        Console.Clear();
+                        Main(args);
+                        break;
+                    case "buble":
+                        Console.WriteLine("Give an array size:");
+                        RunSort(Bubble.Sorting(GetRandomArray(Convert.ToInt32(Console.ReadLine()))));
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operation!");
+                        break;
+                }
+            }
+        }
+
+        private static void RunSort(int[] res)
+        {
+            Console.WriteLine("Output: [{0}]", string.Join(", ", res));
+        }
+
+        private static int[] GetRandomArray(int size)
+        {
+            var array = new int[size];
+            var random = new Random();
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-100, 100);
+            }
+            Console.WriteLine("Input: [{0}]", string.Join(", ", array));
+            return array;
+        }
+
+    }
+}
