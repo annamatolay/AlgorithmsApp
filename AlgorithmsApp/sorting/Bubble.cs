0e3e732e828654bacd8aeb413c2dabdb990016f7
matolaypal <matolaypal@gmail.com>
2017-04-26 00:17:08 +0200

    bubble done

---
+++ AlgorithmsApp/sorting/Bubble.cs
@@ -0,0 +1,20 @@
+﻿namespace AlgorithmsApp.sorting
+{
+    public class Bubble
+    {
+        public static int[] Sorting(int[] array)
+        {
+            for (var i = 0; i < array.Length - 1; i++)
+            {
+                for (var j = 1; j < array.Length - i; j++)
+                {
+                    if (array[j - 1] <= array[j]) continue;
+                    var temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+            return array;
+        }
+    }
+}
