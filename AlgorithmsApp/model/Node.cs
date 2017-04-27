bd7a76cedfbcfecaa3bfc1c11d761b2a413ae0ad
matolaypal <matolaypal@gmail.com>
2017-04-27 13:33:52 +0200

    implemented with generics type

---
+++ AlgorithmsApp/model/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AlgorithmsApp
+{
+    public class Node<T>
+    {
+        private T _data;
+        private Node<T>[] _neig;
+
+        public Node(T data, Node<T>[] neigh = null)
+        {
+            _data = data;
+            _neig = neigh;
+        }
+        public T Data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+
+        public Node<T>[] Neig
+        {
+            get { return _neig; }
+            set { _neig = value; }
+        }
+    }
+}
